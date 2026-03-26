c3075d66becaba230d4b9ca6a69f35438146927d
Faham <m.faham3@gmail.com>
2026-03-26 17:08:25 +0200

    fix(core): fix an issue with http mock and ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
@@ -15,7 +14,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     importProvidersFrom(CoreModule),
     providePrimeNG({
       theme: {
