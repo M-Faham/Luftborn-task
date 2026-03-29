33c4e3b3d2820031a68bf0098c35d990a6867d90
Faham <m.faham3@gmail.com>
2026-03-29 14:48:55 +0200

    feat(analytics): add carousel ui

--- src/app/shared/components/statistics/statistics.component.ts
+++ src/app/shared/components/statistics/statistics.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
 import { Skeleton } from 'primeng/skeleton';
 import { StatisticsService } from '../../../features/dashboard/services/statistics.service';
 import { Statistic } from '../../models';
 import { StatsCardComponent } from '../stats-card/stats-card.component';
-
 @Component({
   selector: 'lb-statistics',
-  imports: [StatsCardComponent, Skeleton],
+  imports: [StatsCardComponent, Skeleton, CarouselModule],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,6 +15,15 @@ import { StatsCardComponent } from '../stats-card/stats-card.component';
 export class StatisticsComponent {
   readonly statistics: Signal<Statistic[]>;
   readonly isLoading: Signal<boolean>;
+
+  readonly skeletonItems = [1, 2, 3, 4];
+
+  readonly responsiveOptions = [
+    { breakpoint: '1400px', numVisible: 4, numScroll: 1 },
+    { breakpoint: '1200px', numVisible: 3, numScroll: 1 },
+    { breakpoint: '990px', numVisible: 2, numScroll: 1 },
+    { breakpoint: '640px', numVisible: 1, numScroll: 1 },
+  ];
 
   private readonly statisticsService = inject(StatisticsService);
   constructor() {
